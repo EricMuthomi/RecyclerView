1e2a0e0c6290e166efe653a0aa07787af38ba7ec
Eric Palmer <muthomiericpalmer@gmail.com>
2022-01-31 15:08:24 +0300

    few changes

--- app/src/main/java/com/ericpalmer/myrecyclerviewapp/MainActivity.java
+++ app/src/main/java/com/ericpalmer/myrecyclerviewapp/MainActivity.java
@@ -33,20 +33,20 @@ public class MainActivity extends AppCompatActivity {
         recyclerView.setLayoutManager(new GridLayoutManager(this,2));
 
         ArrayList<HelperClass> arrayList=new ArrayList<>();
-        arrayList.add(new HelperClass(R.drawable.airforceshoes,"AirForce","This is just a dummy description"));
-        arrayList.add(new HelperClass(R.drawable.airjordanshoes,"AirJordan","This is just a dummy description"));
-        arrayList.add(new HelperClass(R.drawable.bomberjacket,"Bomber Jacket","This is just a dummy description"));
-        arrayList.add(new HelperClass(R.drawable.designertrousers,"Louis Vuitton Trousers","This is just a dummy description"));
-        arrayList.add(new HelperClass(R.drawable.givenchypants,"Givenchy Pants","This is just a dummy description"));
-        arrayList.add(new HelperClass(R.drawable.diorshirts,"Dior Shirts","This is just a dummy description"));
-        arrayList.add(new HelperClass(R.drawable.hat,"Gucci Marvin","This is just a dummy description"));
-        arrayList.add(new HelperClass(R.drawable.heavyjacket,"Prada Jackets","This is just a dummy description"));
-        arrayList.add(new HelperClass(R.drawable.jacket,"Jacket","This is just a dummy description"));
-        arrayList.add(new HelperClass(R.drawable.louisvuittonhat,"Louis Vuitton Marvin","This is just a dummy description"));
+        arrayList.add(new HelperClass(R.drawable.airforceshoes,"AirForce","Quality Military Nike AirForce shoes"));
+        arrayList.add(new HelperClass(R.drawable.airjordanshoes,"AirJordan","Very affordable Air Jordans"));
+        arrayList.add(new HelperClass(R.drawable.bomberjacket,"Bomber Jacket","Bomber jacket for the winter!"));
+        arrayList.add(new HelperClass(R.drawable.designertrousers,"Louis Vuitton Trousers","Synthetic Louis Vuitton trousers"));
+        arrayList.add(new HelperClass(R.drawable.givenchypants,"Givenchy Pants","Quality Givenchy pants for you!"));
+        arrayList.add(new HelperClass(R.drawable.diorshirts,"Dior Shirts","Dior T-Shirts available in all colors"));
+        arrayList.add(new HelperClass(R.drawable.hat,"Gucci Marvin","Black Louis Vuitton marvin"));
+        arrayList.add(new HelperClass(R.drawable.heavyjacket,"Prada Jackets","Synthetic Prada jackets for winter"));
+        arrayList.add(new HelperClass(R.drawable.jacket,"Jacket","Jackets available in all colors and designs"));
+        arrayList.add(new HelperClass(R.drawable.louisvuittonhat,"Louis Vuitton Marvin","Louis Vuitton marvins to kick the cold"));
         arrayList.add(new HelperClass(R.drawable.nikeairshoes,"Nike Air Force Shoes","This is just a dummy description"));
-        arrayList.add(new HelperClass(R.drawable.pradaboots,"Prada Boots","This is just a dummy description"));
-        arrayList.add(new HelperClass(R.drawable.sweatpants,"Sweatpants","This is just a dummy description"));
-        arrayList.add(new HelperClass(R.drawable.tshirts,"T-Shirts","This is just a dummy description"));
+        arrayList.add(new HelperClass(R.drawable.pradaboots,"Prada Boots","Prada Boots to color your look!"));
+        arrayList.add(new HelperClass(R.drawable.sweatpants,"Sweatpants","A whole set is available to color your look!"));
+        arrayList.add(new HelperClass(R.drawable.tshirts,"T-Shirts","Louis V T-Shirts available in all colors and sizes!"));
 
         myAdapter=new AdapterClass(arrayList);
         recyclerView.setAdapter(myAdapter);
